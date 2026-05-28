171bfe0fd2bd135759092137452c74e6f8475119
Joost de Folter <folterj@gmail.com>
2026-05-28 16:19:50 +0200

    Improve sub-field rocrate

--- src/rocrate_utils.py
+++ src/rocrate_utils.py
@@ -3,7 +3,7 @@
 # https://github.com/clbarnes/rembi-mifa-py/blob/main/examples/rembi.py
 
 
-from rocrate.model import ComputationalWorkflow
+from rocrate.model import ComputationalWorkflow, ContextEntity
 
 from src.rembi_extension import ImageAcquisition
 from src.zarr_extension import ZarrCrate
@@ -16,11 +16,12 @@ def create_ro_crate(source, dest_path={}):
     properties['name'] = source.get_name()
     #properties["description"] = source.get_description()
     #properties["license"] = source.get_license()
-    crate.add_dataset(dest_path='.', properties=properties)
+    zarr_root = crate.add_dataset(dest_path='.', properties=properties)
 
     acquisition_properties = {
         'fbbi_id': {'@id': 'obo:FBbi_00000257'},
-        'instrument': {
+        'instrument':
+        {
             "@id": "#microscope-001",
             "@type": "IndividualProduct",
             "name": "Zeiss LSM 900",
@@ -28,21 +29,40 @@ def create_ro_crate(source, dest_path={}):
                 "@id": "https://ror.org"
             },
             "serialNumber": "12345-XYZ"
-        },
-        'additionalProperty': [
-            {
-                "@id": "#acq:001",
-                "@type": "PropertyValue",
-                "name": "MeanBeamCharge",
-                "value": "1.0"
-            },
-        ]
+        }
     }
+
+    instrument_properties = {'instrument':
+        {
+            "@id": "#microscope-001",
+            "@type": "IndividualProduct",
+            "name": "Zeiss LSM 900",
+            "manufacturer": {
+                "@id": "https://ror.org"
+            },
+            "serialNumber": "12345-XYZ"
+        }
+    }
+    instrument_entity = ContextEntity(crate, properties=instrument_properties)
+
+    additional_properties = {
+        'additionalProperty': [
+        {
+            "@id": "#acq:001",
+            "@type": "PropertyValue",
+            "name": "MeanBeamCharge",
+            "value": "1.0"
+        }
+    ]}
+
     # TODO: Consider hasDefinedTerm as a better alternative when using a defined ontology?
     # TODO: Can add variableMeasured for output properties
 
+    acquisition_entity = ImageAcquisition(crate, identifier="#acquisition-001", properties=acquisition_properties)
+
     # add to acquisition_properties from source
-    crate.add(ImageAcquisition(crate, properties=acquisition_properties))
+    crate.add(acquisition_entity)
+    zarr_root["resultOf"] = acquisition_entity
 
 #    crate.add(ComputationalWorkflow(crate, workflow_schema_filename))
 
